e4ef02833935e1c5e2b5e8780f634be4735768d3
PADIDAR-CO <chelongar28@gmail.com>
2025-07-24 00:27:40 -0400

    test

--- main.py
+++ main.py
@@ -49,13 +49,40 @@ dataset = PCamDataset("camelyonpatch_level_2_split_test_x.h5", transform=transfo
 dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
 
 
-class Generator(nn.Module):
-    def __int__(self, dim_z):
-        super(Generator, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=dim_z, out_channels=16, kernel_size=3, padding=1)
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        # self.fc1 = nn.Linear()
+        self.fc1 = nn.Linear(8*24*24, 128)
+        self.fc2 = nn.Linear(128, 2)
+        self.relu = nn.ReLU()
 
-    def forward(self):
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool2(x)
+        print(x.shape)
+        x = x.flatten()
+        print(x.shape)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        print(x.shape)
+        x = F.softmax(x, dim=0)
+
+        return x
+
+
+
+
+
+model = Discriminator()
+x = dataset.__getitem__(10)
+model.forward(x)
