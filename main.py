659ced38e7dbc8bfc7342408f92e1729487d1b6c
PADIDAR-CO <chelongar28@gmail.com>
2025-07-25 21:34:37 -0400

    best model

--- main.py
+++ main.py
@@ -12,47 +12,36 @@ import matplotlib.animation as animation
 from IPython.display import HTML
 
 
-# # Open test images
-# with h5py.File('camelyonpatch_level_2_split_test_x.h5', 'r') as f:
-#     print(list(f.keys()))   # Should show one key, like ['x']
-#     x_test = f['x'][:]      # Load the image array
-#
-# print(f"x_test shape: {x_test.shape}")  # Expected: (num_images, 96, 96, 3)
-#
-# plt.imshow(x_test[150])  # RGB image
-# plt.axis("off")
-# plt.show()
-device = 'cuda'
 
-class PCamDataset(Dataset):
-    def __init__(self, h5_path, transform=None):
-        self.h5_file = h5py.File(h5_path, 'r')
-        self.images = self.h5_file['x']
+device = 'cuda'
+z_dim = 128
+batch_size = 512
+import glob
+class PNGFolderDataset(Dataset):
+    def __init__(self, folder, transform=None):
+        self.paths = sorted(glob.glob(os.path.join(folder, '*.png')))
         self.transform = transform
 
     def __len__(self):
-        return len(self.images)
+        return len(self.paths)
 
     def __getitem__(self, idx):
-        img = self.images[idx]  # shape: (H, W, C) usually
-
+        img = Image.open(self.paths[idx]).convert('RGB')
         if self.transform:
             img = self.transform(img)
-
         return img
 
 
 transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize(96),
-    transforms.CenterCrop(96),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5]*3, [0.5]*3)
+    transforms.Resize(96),            # works on PIL
+    transforms.CenterCrop(96),        # works on PIL
+    transforms.ToTensor(),            # PIL→FloatTensor in [0,1]
+    transforms.Normalize([0.5]*3,     # now works on Tensor
+                         [0.5]*3)
 ])
 
-dataset = PCamDataset("camelyonpatch_level_2_split_train_x.h5", transform=transform)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-
+dataset = PNGFolderDataset("/content/healthy_images", transform=transform)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -70,23 +59,29 @@ class Discriminator(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1, bias=False)
 
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
 
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(256)
 
         self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(512)
 
         self.conv5 = nn.Conv2d(in_channels=512, out_channels=1, kernel_size=6, stride=1, padding=0, bias=False)
 
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU(0.2)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu(x)
         x = self.conv2(x)
+        x = self.bn2(x)
         x = self.relu(x)
         x = self.conv3(x)
+        x = self.bn3(x)
         x = self.relu(x)
         x = self.conv4(x)
+        x = self.bn4(x)
         x = self.relu(x)
         x = self.conv5(x)
         x = x.view(x.size(0), -1)
@@ -96,23 +91,23 @@ class Discriminator(nn.Module):
 
 class Generator(nn.Module):
 
-    def __init__(self, z_dim=100, g_channels=64, out_channels=3):
+    def __init__(self, z_dim=100, g_channels=128, out_channels=3):
             super().__init__()
 
             # input Z: (z_dim, 1, 1)
             self.convT1 = nn.ConvTranspose2d(z_dim, g_channels * 8, 6, 1, 0, bias=False)  # 6x6
             self.bn1 = nn.BatchNorm2d(g_channels * 8)
 
-            self.convT2 = nn.ConvTranspose2d(g_channels * 8, g_channels * 4, 4, 2, 1, bias=False)  # 6x6
+            self.convT2 = nn.ConvTranspose2d(g_channels * 8, g_channels * 4, 4, 2, 1, bias=False)  # 12x12
             self.bn2 = nn.BatchNorm2d(g_channels * 4)
 
-            self.convT3 = nn.ConvTranspose2d(g_channels * 4, g_channels * 2, 4, 2, 1, bias=False)  # 6x6
+            self.convT3 = nn.ConvTranspose2d(g_channels * 4, g_channels * 2, 4, 2, 1, bias=False)  # 24x24
             self.bn3 = nn.BatchNorm2d(g_channels * 2)
 
-            self.convT4 = nn.ConvTranspose2d(g_channels * 2, g_channels, 4, 2, 1, bias=False)  # 6x6
+            self.convT4 = nn.ConvTranspose2d(g_channels * 2, g_channels, 4, 2, 1, bias=False)  # 48x48
             self.bn4 = nn.BatchNorm2d(g_channels)
 
-            self.convT5 = nn.ConvTranspose2d(g_channels, out_channels, 4, 2, 1, bias=False)  # 6x6
+            self.convT5 = nn.ConvTranspose2d(g_channels, out_channels, 4, 2, 1, bias=False)  # 96x96
             self.tanh = nn.Tanh()
             self.relu = nn.ReLU()
 
@@ -134,12 +129,13 @@ class Generator(nn.Module):
             return z
 
 
-generator = Generator().to(device)
+generator = Generator(z_dim= z_dim).to(device)
 discriminator = Discriminator().to(device)
 model = Discriminator()
 
 criterion = nn.BCELoss()
-fixed_noise = torch.randn(32, 100, 1, 1, device=device) #(batch, channels, h, w)
+fixed_noise = torch.randn(batch_size, z_dim, 1, 1, device=device) #(batch, channels, h, w)
+fixed_noise = torch.empty(batch_size, z_dim, 1, 1, device=device).uniform_(-1, 1)
 
 real_label = 1.
 fake_label = 0.
@@ -154,7 +150,8 @@ optimizerD = optim.Adam(discriminator.parameters(), lr=2e-4, betas=(0.5, 0.999))
 img_list = []
 G_losses = []
 D_losses = []
-epochs = 1
+best_g_loss = float('inf')
+epochs = 60
 iters = 0
 for epoch in range(epochs):
     for i, data in enumerate(dataloader, 0):
@@ -169,7 +166,9 @@ for epoch in range(epochs):
         D_x = output.mean().item()
 
         ## Train using fake batch
-        noise = torch.randn(batch, 100, 1, 1, device=device)
+        # noise = torch.randn(batch, z_dim, 1, 1, device=device)
+        noise = torch.empty(batch, z_dim, 1, 1, device=device).uniform_(-1, 1)
+
         fake = generator(noise)
         label.fill_(fake_label)
         output = discriminator(fake.detach()).view(-1)
@@ -206,29 +205,16 @@ for epoch in range(epochs):
                 fake = generator(fixed_noise).detach().cpu()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
+        current_g_loss = errG.item()
+
+        # Check if this is the best generator loss so far
+
+        best_g_loss = current_g_loss
+
+            # Save generator & discriminator state_dicts
+        torch.save(generator.state_dict(), 'best_generator_healthy.pth')
+        torch.save(discriminator.state_dict(), 'best_discriminator_healthy.pth')
+
+
         iters += 1
 
-
-fig = plt.figure(figsize=(8,8))
-plt.axis("off")
-ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
-ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
-
-HTML(ani.to_jshtml())
-
-# Grab a batch of real images from the dataloader
-real_batch = next(iter(dataloader))
-
-# Plot the real images
-plt.figure(figsize=(15,15))
-plt.subplot(1,2,1)
-plt.axis("off")
-plt.title("Real Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
-
-# Plot the fake images from the last epoch
-plt.subplot(1,2,2)
-plt.axis("off")
-plt.title("Fake Images")
-plt.imshow(np.transpose(img_list[-1],(1,2,0)))
-plt.show()
